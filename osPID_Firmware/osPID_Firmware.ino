59bd088a2becff8b6fdac53d449fb63196c14f71
Benjamin K. Stuhl <stuhl@umd.edu>
2013-05-24 18:25:00 -0400

    save a couple hundred byte of code size

--- osPID_Firmware/osPID_Firmware.ino
+++ osPID_Firmware/osPID_Firmware.ino
@@ -475,9 +475,9 @@ void loop()
     {
       // a complete command has been received
       serialCommandBuffer[serialCommandLength] = '\0';
+      drawNotificationCursor('*');
       processSerialCommand();
       serialCommandLength = 0;
-      drawNotificationCursor('*');
     }
   }
 }
